f72299a15bc35842d68c96111e00e39c10cf8b3a
EC2 Default User <ec2-user@ip-172-31-18-193.ec2.internal>
2025-04-21 12:06:28 +0000

    changes made to copy_war script

--- scripts/copy_war.sh
+++ scripts/copy_war.sh
@@ -5,11 +5,15 @@ echo "📦 Copying WAR file to Tomcat webapps..."
 
 WAR_SOURCE=$(find /opt/codedeploy-agent/deployment-root -name "nextwork-web-project.war" | head -n 1)
 WAR_DEST="/opt/tomcat/webapps/nextwork-web-project.war"
+APP_FOLDER="/opt/tomcat/webapps/nextwork-web-project"
 
 if [ -f "$WAR_SOURCE" ]; then
+  echo "🧹 Removing old app folder (if exists)..."
+  sudo rm -rf "$APP_FOLDER"
+
+  echo "✅ Copying WAR..."
   cp "$WAR_SOURCE" "$WAR_DEST"
   chown tomcat: "$WAR_DEST"
-  echo "✅ WAR copied to $WAR_DEST"
 else
   echo "❌ WAR file not found at $WAR_SOURCE"
   exit 1
